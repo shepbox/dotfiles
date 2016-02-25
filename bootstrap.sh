cbc7f2c103bd302d6aabfd04084c1b88da1142b7
Sam Shepherd <sam@shepbox.com>
2016-02-25 17:01:58 -0600

    Bootstrap now handles submodules.

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,21 +2,50 @@
 
 cd "$(dirname "${BASH_SOURCE}")";
 
-git pull origin master;
-
 function doIt() {
 	rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" \
 		--exclude "README.md" --exclude "LICENSE-MIT.txt" --exclude ".gitmodules" -avh --no-perms . ~;
 	source ~/.bash_profile;
 }
 
-if [ "$1" == "--force" -o "$1" == "-f" ]; then
-	doIt;
-else
-	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
+function submodules() {
+	read -p "Init submodules? (first time only) (y/N) " -n 1;
 	echo "";
 	if [[ $REPLY =~ ^[Yy]$ ]]; then
-		doIt;
+		git submodule init;
+		git submodule update;
+		git submodule foreach git checkout master;
+	else
+		updateSubmodules;
 	fi;
-fi;
+}
+
+function updateSubmodules() {
+	git submodule foreach git pull;
+}
+
+function main() {
+	git pull origin master;
+
+	submodules;
+
+	if [ "$1" == "--force" -o "$1" == "-f" ]; then
+		doIt;
+	else
+		read -p "This may overwrite existing files in your home directory. Are you sure? (y/N) " -n 1;
+		echo "";
+		if [[ $REPLY =~ ^[Yy]$ ]]; then
+			echo "Going for it!";
+			doIt;
+		else
+			echo "Chicken'd out!";
+		fi;
+	fi;
+}
+
+main;
+
 unset doIt;
+unset main;
+unset submodules;
+unset updateSubmodules;
